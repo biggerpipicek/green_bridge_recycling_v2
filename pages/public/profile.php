d7f002dfaeadaeca8f39585f26cd7a9a5d4a06f2
biggerpipicek <kczay123@gmail.com>
2026-04-17 12:17:24 +0200

    profile template start

--- pages/public/profile.php
+++ pages/public/profile.php
@@ -6,6 +6,31 @@
     include "../../build/header.php";
     ?>
 
+    <div class="container-fluid">
+        <div class="container-md">
+            <div class="row">
+                <div class="col d-flex align-items-center">
+                    <img src="../../imgs/user.png" alt="Profile picture" style="width: 115px; height: 110px;">
+                    <div class="together ms-4">
+                        <b>test_user</b><br>
+                        <i>phillips.m@greenbridgerecycling.com</i>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col">
+                    <label for="username" class="form-label">Username</label>
+                    <p>test_user</p>
+                </div>
+                <div class="col">
+                    <label for="email" class="form-label">E-mail</label>
+                    <p>phillips.m@greenbridgerecycling.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php
         include "../../build/footer.php";
     ?>

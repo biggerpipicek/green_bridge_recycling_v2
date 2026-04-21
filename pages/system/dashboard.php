83441b6c1c75f67908f1648afc8a451c6f7d94ed
biggerpipicek <kczay123@gmail.com>
2026-04-21 15:59:48 +0200

    dashboard

--- pages/system/dashboard.php
+++ pages/system/dashboard.php
@@ -6,11 +6,39 @@
     require "../../build/functions.php";
     $page_title = "GBR Dashboard";
     include "../../build/header.php";
+
+    // CASE SWITCHES
+    $filter = $_GET['filter'] ?? 'monthly';
+    switch($filter) {
+        case 'weekly':
+            break;
+
+        case 'monthly':
+            break;
+        
+        case 'semi-annually':
+            break;
+        
+        case 'annually':
+            break;
+
+        case 'user-selected':
+            break;
+    }
     ?>
 
     <div class="container-fluid">
         <div class="container-sm">
-            
+            <form action="" method="get">
+                <label for="filter" class="form-label">Select</label>
+                <select name="filter" class="form-select">
+                    <option value="weekly">Weekly</option>
+                    <option value="monthly">Monthly</option>
+                    <option value="semi-annually">Semi-Annualy</option>
+                    <option value="annually">Annually</option>
+                    <option value="user-selected">User Selected</option>
+                </select>
+            </form>
         </div>
     </div>
 
